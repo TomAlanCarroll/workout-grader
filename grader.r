aaad47a41c92311a8d883ac02e66c5ba057f6752
Tom Carroll <tomalancarroll@gmail.com>
2017-03-19 23:17:23 +0100

    Added training data partitions

--- grader.r
+++ grader.r
@@ -4,11 +4,12 @@ library(randomForest)
 args <- commandArgs(TRUE)
 
 trainingDataDir <- "sensor-data"
-trainingFile <- paste(trainingDataDir, "training.csv", sep="/")
-testingFile <- paste(trainingDataDir, "testing.csv", sep="/")
+trainingFile <- paste(trainingDataDir, "training.csv", sep = "/")
+testingFile <- paste(trainingDataDir, "testing.csv", sep = "/")
 trainingFileUrl <- args[1]
 testingFileUrl <- args[2]
 numColumns <- 160
+trainTestRatio <- 0.75
 
 if(length(args) < 2) {
 	stop("Please supply training CSV URL as first command argument & testing CSV as second command argument.")
@@ -24,13 +25,18 @@ if (!file.exists(trainingFile) || !file.exists(testingFile)) {
 	print(paste("Downloading training file:", trainingFileUrl))
 	print(paste("Downloading testing file:", testingFileUrl))
 
-	download.file(trainingFileUrl, destfile=trainingFile)
-	download.file(testingFileUrl, destfile=testingFile)
+	download.file(trainingFileUrl, destfile = trainingFile)
+	download.file(testingFileUrl, destfile = testingFile)
 }
 
 if (dim(read.csv(trainingFile))[2] != numColumns || dim(read.csv(testingFile))[2] != numColumns) {
 	stop(paste(numColumns, " columns must be present in training and testing CSV"))
 }
 
-# Load the files. Skip the first 7 columns (not sensor/class data)
-training <- read.csv(trainingFile, na.strings = c("NA", ""), colClasses=c("NULL","NULL","NULL","NULL","NULL","NULL","NULL",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+# Load the files
+trainingRaw <- read.csv(trainingFile, na.strings = c("NA", ""))
+
+trainingPartition <- createDataPartition(y = trainingRaw$classe, p = trainTestRatio, list = FALSE)
+training <- trainingRaw[trainingPartition,]
+testing <- trainingRaw[-trainingPartition,]
+
